4ecc949cbdd4f1ae290921cb7f18f02ab8251b60
Hemaseenu <146916627+Hemaseenu@users.noreply.github.com>
2026-06-22 11:36:07 +0530

    Add files via upload

---
+++ student.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const StudentSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    department: String
+});
+
+module.exports = mongoose.model('Student', StudentSchema);
